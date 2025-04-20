f500f0e9196e54a98ff374ea803ce9b9b35431aa
fschonfelder <fschoenfleder@etu-unsitra.fr>
2025-04-20 23:36:28 +0200

    footer done

---
+++ assets/php/footer.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="/assets/css/footer.css">
+<footer>
+    <div class="footer-container">
+        <!-- Section Contact -->
+        <div class="contact-info footer-part">
+            <h3>Contact</h3>
+            <p>Email : <a href="mailto:franck.redhood@gmail.com">franck.redhood@gmail.com</a></p>
+            <p>GitHub :<a href="https://github.com/fschoenfelder113" target="_blank">github.com/fschoenfelder113</a></p>
+        </div>
+        <!-- Section Glossaire & Ressources -->
+        <div class="glossary footer-part">
+            <h3>Glossaire & Aide</h3>
+            <ul>
+                <li><a href="https://developer.mozilla.org/fr/" target="_blank">MDN Web Docs</a> – Documentation complète sur HTML/CSS/JS</li>
+                <li><a href="https://www.w3schools.com/" target="_blank">W3Schools</a> – Site d'apprentissage</li>
+                <li><p>Microsoft copilot - IA intégé de w11 et Microsoft edge </p></li>
+            </ul>
+        </div>
+
+        <!-- Section Mention Légale -->
+        <div class="legal footer-part">
+            <h3>Mention Légale</h3>
+            <p>&copy; 2025 Schoenfelder Franck - Portfolio étudiant, tous droits réservés.</p>
+        </div>
+    </div>
+</footer>
