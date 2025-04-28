bf5800d1a7443db5b0220c5b94ba4730b9ad6a7b
fschonfelder <fschoenfleder@etu-unsitra.fr>
2025-04-28 09:47:34 +0200

    addedFillinFileExample

---
+++ assets/php/tabProjets.php
@@ -0,0 +1,47 @@
+<?php
+$projets = [
+        [
+                "fr" => [
+                        "nom" => "Carnet de Conduite",
+                        "resume" => "Un système interactif pour enregistrer et visualiser les trajets en fonction des véhicules.",
+                        "details" => "Ce projet de carnet de conduite permet de gérer les trajets liés à différents véhicules en utilisant le stockage local du navigateur (LocalStorage). Il inclut des fonctionnalités dynamiques comme l'ajout de véhicules, la saisie de trajets avec des détails personnalisés (date, distance, météo, état du trafic, etc.), et un affichage du bilan total des trajets. L'interface est conçue pour être accessible et intuitive, avec des formulaires dynamiques et des mises à jour en temps réel des données saisies.",
+                ],
+                "en" => [
+                        "nom" => "Driving Logbook",
+                        "resume" => "An interactive system to log and visualize trips based on vehicles.",
+                        "details" => "This driving logbook project helps manage trips linked to different vehicles using the browser's local storage (LocalStorage). It features dynamic functionalities such as adding vehicles, recording trips with customized details (date, distance, weather, traffic conditions, etc.), and displaying a complete trip summary. The interface is designed to be accessible and intuitive, with dynamic forms and real-time data updates.",
+                ],
+                "technologies" => ["HTML", "CSS", "JavaScript", "LocalStorage"],
+                "link" => [
+                        [
+                                "label" => "download-project",
+                                "url" => "assets/downloads/projet/conduite/carnet_conduite.zip"
+                        ]
+                ],
+                "image" => [
+                        [
+                                "label" => [
+                                        "fr" => "Page 1",
+                                        "en" => "Page 1"
+                                ],
+                                "path" => "assets/images/projet/conduite/page1.png"
+                        ],
+                        [
+                                "label" => [
+                                        "fr" => "Page 2",
+                                        "en" => "Page 2"
+                                ],
+
+                                "path" => "assets/images/projet/conduite/page2.png"
+                        ],
+                        [
+                                "label" => [
+                                        "fr" => "Exemple de code",
+                                        "en" => "Code example"
+                                ],
+
+                                "path" => "assets/images/projet/conduite/code.png"
+                        ]
+                ]       
+        ]
+];
